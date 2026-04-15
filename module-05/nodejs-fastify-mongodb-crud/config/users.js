e1f82941338c861ab3b8385b221c93597af7ca1e
Leonardo Fonseca <leonardofonsecasilva@hotmail.com>
2026-04-15 16:49:40 -0300

    add: reference project to be converted to MCP in module-05

---
+++ module-05/nodejs-fastify-mongodb-crud/config/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    name: 'John Doe',
+    phone: '123456789',
+  },
+  {
+    name: 'Jane Doe',
+    phone: '987654321',
+  },
+  {
+    name: 'Chapolin Colorado',
+    phone: '123456789',
+  }
+]
+
+export { users }
